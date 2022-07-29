5a0a9836cf805bd18bcc918105593bd43aaa7060
asaduzzaman69 <=>
2022-07-29 16:28:52 +0600

    navbar created

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Banner from './Banner';
+
+const Home = () => {
+    return (
+        <div>
+            <h2 className='text-primary'>Home</h2>
+            <Banner></Banner>
+
+        </div>
+    );
+};
+
+export default Home;
